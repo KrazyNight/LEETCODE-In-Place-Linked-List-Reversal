850102ece77a13e1a8d90da1f6604b85e10fa02b
Erik Nieto Reynoso <erik.nieto101@gmail.com>
2025-07-23 19:35:15 -0700

    leetcode template

---
+++ app.js
@@ -0,0 +1,433 @@
+//Leetcode: 206. Reverse Linked List 
+
+/** Original Question
+206. Reverse Linked List
+
+Given the head of a singly linked list, reverse the list, 
+and return the reversed list.
+
+ 
+
+Example 1:
+Input: head = [1,2,3,4,5]
+Output: [5,4,3,2,1]
+
+Example 2:
+Input: head = [1,2]
+Output: [2,1]
+
+Example 3:
+Input: head = []
+Output: []
+
+ */
+
+/** Q: Template
+var reverseList = function(head) {
+    
+};
+ */
+
+
+
+
+
+
+//Leetcode: 206. Reverse Linked List 
+
+/** Original Question
+206. Reverse Linked List
+
+Given the head of a singly linked list, reverse the list, 
+and return the reversed list.
+
+ 
+
+Example 1:
+Input: head = [1,2,3,4,5]
+Output: [5,4,3,2,1]
+
+Example 2:
+Input: head = [1,2]
+Output: [2,1]
+
+Example 3:
+Input: head = []
+Output: []
+
+ */
+
+/** Q: Template
+var reverseList = function(head) {
+    
+};
+
+ */
+
+
+
+
+
+//Leetcode: 206. Reverse Linked List 
+
+/** Original Question
+206. Reverse Linked List
+
+Given the head of a singly linked list, reverse the list, 
+and return the reversed list.
+
+ 
+
+Example 1:
+Input: head = [1,2,3,4,5]
+Output: [5,4,3,2,1]
+
+Example 2:
+Input: head = [1,2]
+Output: [2,1]
+
+Example 3:
+Input: head = []
+Output: []
+
+ */
+
+/** Q: Template
+ * 
+var reverseList = function(head) {
+    
+};
+ */
+
+
+
+
+
+
+
+
+
+
+//Leetcode: 143. Reorder List 
+
+/** Original Question
+143. Reorder List
+
+You are given the head of a singly linked-list. The list can be represented as:
+
+L0 → L1 → … → Ln - 1 → Ln
+
+Reorder the list to be on the following form:
+
+L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → …
+
+You may not modify the values in the list's nodes. 
+Only nodes themselves may be changed.
+
+
+Example 1:
+Input: head = [1,2,3,4]
+Output: [1,4,2,3]
+
+Example 2:
+Input: head = [1,2,3,4,5]
+Output: [1,5,2,4,3]
+ 
+ */
+
+/** Q: Template
+var reorderList = function(head) {
+    
+};
+ */
+
+
+
+
+
+
+//Leetcode: 143. Reorder List 
+
+/** Original Question
+143. Reorder List
+
+You are given the head of a singly linked-list. The list can be represented as:
+
+L0 → L1 → … → Ln - 1 → Ln
+
+Reorder the list to be on the following form:
+
+L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → …
+
+You may not modify the values in the list's nodes. 
+Only nodes themselves may be changed.
+
+
+Example 1:
+Input: head = [1,2,3,4]
+Output: [1,4,2,3]
+
+Example 2:
+Input: head = [1,2,3,4,5]
+Output: [1,5,2,4,3]
+ */
+
+/** Q: Template
+var reorderList = function(head) {
+    
+};
+ */
+
+
+
+
+
+
+//Leetcode: 143. Reorder List 
+
+/** Original Question
+143. Reorder List
+
+You are given the head of a singly linked-list. The list can be represented as:
+
+L0 → L1 → … → Ln - 1 → Ln
+
+Reorder the list to be on the following form:
+
+L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → …
+
+You may not modify the values in the list's nodes. 
+Only nodes themselves may be changed.
+
+
+Example 1:
+Input: head = [1,2,3,4]
+Output: [1,4,2,3]
+
+Example 2:
+Input: head = [1,2,3,4,5]
+Output: [1,5,2,4,3]
+
+ */
+
+/** Q: Template
+var reorderList = function(head) {
+    
+};
+ */
+
+
+
+
+
+
+
+
+
+
+//Leetcode: 25. Reverse Node in K-Group
+
+/** Original Question
+25. Reverse Nodes in k-Group
+
+- Given the head of a linked list, reverse the nodes 
+of the list k at a time, and return the modified list.
+
+- k is a positive integer and is less than or equal
+to the length of the linked list. If the number of nodes
+is not a multiple of k then left-out nodes, in the end, should remain as it is.
+
+- You may not alter the values in the list's nodes, 
+only nodes themselves may be changed.
+
+ 
+
+Example 1:
+Input: head = [1,2,3,4,5], k = 2
+Output: [2,1,4,3,5]
+
+Example 2:
+Input: head = [1,2,3,4,5], k = 3
+Output: [3,2,1,4,5]
+
+ */
+
+/** Q: Template
+var reverseKGroup = function(head, k) {
+    
+};
+ */
+
+
+
+
+
+
+//Leetcode: 25. Reverse Node in K-Group
+
+/** Original Question
+25. Reverse Nodes in k-Group
+
+- Given the head of a linked list, reverse the nodes 
+of the list k at a time, and return the modified list.
+
+- k is a positive integer and is less than or equal
+to the length of the linked list. If the number of nodes
+is not a multiple of k then left-out nodes, in the end, should remain as it is.
+
+- You may not alter the values in the list's nodes, 
+only nodes themselves may be changed.
+
+ 
+
+Example 1:
+Input: head = [1,2,3,4,5], k = 2
+Output: [2,1,4,3,5]
+
+Example 2:
+Input: head = [1,2,3,4,5], k = 3
+Output: [3,2,1,4,5]
+
+ */
+
+/** Q: Template
+var reverseKGroup = function(head, k) {
+    
+};
+ */
+
+
+
+
+
+
+//Leetcode: 25. Reverse Node in K-Group
+
+/** Original Question
+25. Reverse Nodes in k-Group
+
+- Given the head of a linked list, reverse the nodes 
+of the list k at a time, and return the modified list.
+
+- k is a positive integer and is less than or equal
+to the length of the linked list. If the number of nodes
+is not a multiple of k then left-out nodes, in the end, should remain as it is.
+
+- You may not alter the values in the list's nodes, 
+only nodes themselves may be changed.
+
+ 
+
+Example 1:
+Input: head = [1,2,3,4,5], k = 2
+Output: [2,1,4,3,5]
+
+Example 2:
+Input: head = [1,2,3,4,5], k = 3
+Output: [3,2,1,4,5]
+
+ */
+
+/** Q: Template
+var reverseKGroup = function(head, k) {
+    
+};
+ */
+
+
+
+
+
+
+
+
+
+
+//Leetcode: 92. Reverse Link List
+
+/** Original Question
+92. Reverse Linked List II
+
+Given the head of a singly linked list and two integers left and right 
+where left <= right, reverse the nodes of the list from position left 
+to position right, and return the reversed list.
+
+ 
+
+Example 1:
+Input: head = [1,2,3,4,5], left = 2, right = 4
+Output: [1,4,3,2,5]
+
+Example 2:
+Input: head = [5], left = 1, right = 1
+Output: [5]
+ 
+ */
+
+/** Q: Template
+var reverseBetween = function(head, left, right) {
+    
+};
+ */
+
+
+
+
+
+//Leetcode: 92. Reverse Link List
+
+/** Original Question
+92. Reverse Linked List II
+
+Given the head of a singly linked list and two integers left and right 
+where left <= right, reverse the nodes of the list from position left 
+to position right, and return the reversed list.
+
+ 
+
+Example 1:
+Input: head = [1,2,3,4,5], left = 2, right = 4
+Output: [1,4,3,2,5]
+
+Example 2:
+Input: head = [5], left = 1, right = 1
+Output: [5]
+
+ */
+
+/** Q: Template
+var reverseBetween = function(head, left, right) {
+    
+};
+ */
+
+
+
+
+
+//Leetcode: 92. Reverse Link List
+
+/** Original Question
+92. Reverse Linked List II
+
+Given the head of a singly linked list and two integers left and right 
+where left <= right, reverse the nodes of the list from position left 
+to position right, and return the reversed list.
+
+ 
+
+Example 1:
+Input: head = [1,2,3,4,5], left = 2, right = 4
+Output: [1,4,3,2,5]
+
+Example 2:
+Input: head = [5], left = 1, right = 1
+Output: [5]
+ */
+
+/** Q: Template
+var reverseBetween = function(head, left, right) {
+    
+};
+ */
+
